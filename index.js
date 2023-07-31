043d76ae3d42846d3cacd49dcfbd6b72283aebf3
philip cleaver <socks4penguins@gmail.com>
2023-07-31 11:52:01 +0700

    imports tested, writing csv, listening

---
+++ index.js
@@ -0,0 +1,45 @@
+const csvWriter = require('csv-write-stream')
+const { OS2LServer } = require("os2l");
+const fs = require("fs");
+
+var writer = csvWriter({ headers: ["hello", "foo"] })
+
+// All options are optional
+let server = new OS2LServer({
+    port: 5000 // TCP Port to listen on
+});
+
+// Register events
+server.on("error", err => {
+    console.error(err);
+});
+
+server.on("btnOn", name => {
+    if (name == "fog") {
+        // ... code for starting a fog machine
+        server.feedback("fog", "on");
+    }
+});
+
+server.on("btnOff", name => {
+    if (name == "fog") {
+        // ... code for stopping a fog machine
+        server.feedback("fog", "off");
+    }
+});
+
+server.on("beat", data => {
+    console.log('beat:', JSON.stringify(data))
+    // Toggle light or something
+});
+
+// Start the server
+server.start().then(() => {
+    console.log("Server is now listening on port: ", server.port);
+});
+
+writer.pipe(fs.createWriteStream('out.csv'))
+console.log('writing test file')
+writer.write(['world', 'bar'])
+console.log('written out.csv')
+writer.end()
